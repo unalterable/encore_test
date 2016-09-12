77e746e77a48f1c7f8a7f86c55c2283e754305b9
Unalterable <diamond.oliver@gmail.com>
2016-09-12 17:38:00 +0100

    finishing

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 $(function(){
 
+  var URL = {url: "https://api.spotify.com/v1/artists/6futYSDVulYR2PktBjTB5W/top-tracks?country=gb"};
 
   $("#fetch-songs").click(function(){
-    var URL = {url: "https://api.spotify.com/v1/artists/6futYSDVulYR2PktBjTB5W/top-tracks?country=gb"};
     appendLoadingRow();
-    expandHiddenFrame();
-    $.ajax( URL ).done( addTracksToSongList );
+    expandSongsFrame();
+    $.ajax( URL ).done( function(data){
+      addTracksToSongList(10, data)
+    });
   })
 
-  function addTracksToSongList( data ) {
-    topTenTracksFromJSON( data ).forEach( appendTrack );
-    removeLoadingRow();
+  function addTracksToSongList( amount, data ) {
+    removeTrackViews();
+    topTracksFromJSON( amount, data ).forEach( appendTrack );
   }
 
-  function topTenTracksFromJSON( data ){
-    return data.tracks.slice(0, 10);
+  function topTracksFromJSON( amount, data ){
+    return data.tracks.slice(0, amount);
   }
 
   function appendTrack( track ){
@@ -25,12 +27,18 @@ $(function(){
     $('#song-list-table').append( views.loadingView() );
   }
 
-  function removeLoadingRow(){
-    $('.loading-row').remove();
+  function removeTrackViews(){
+    $('#song-list-table').empty();
   }
 
-  function expandHiddenFrame(){
-    $("#hidden-frame").css("height", "260px");
+  function expandSongsFrame(){
+    $("#song-list-frame").css("height", "260px");
   }
+
+  appendLoadingRow();
+  $.ajax( URL ).done( function(data){
+    addTracksToSongList(5, data)
+  });
+
 
 });
